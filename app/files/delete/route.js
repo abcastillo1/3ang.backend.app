d2cbafb2ec3654de988010a7a00246008f76a5d1
Ariel Castillo <acastilloariel@gmail.com>
2026-03-27 01:17:44 -0500

    feat: Add logging for document deletion handler to track user and document ID for improved debugging

--- app/files/delete/route.js
+++ app/files/delete/route.js
@@ -22,7 +22,7 @@ async function handler(req, res, next) {
   const { data } = req.body;
   const { user } = req;
   const { AuditDocument } = modelsInstance.models;
-
+  console.log('data', data.id, user.organizationId);
   const document = await AuditDocument.findOne({
     where: { id: data.id, organizationId: user.organizationId }
   });
